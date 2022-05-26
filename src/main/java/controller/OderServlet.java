a1ec3f39f68f691371b52785d2d0feed67231d4f
Le van son <levansoon>
2022-05-26 16:33:38 +0700

    son3

--- src/main/java/controller/OderServlet.java
+++ src/main/java/controller/OderServlet.java
@@ -1,9 +1,11 @@
+package controller;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "OderServlet", value = "/OderServlet")
+@WebServlet(name = "controller.OderServlet", value = "/controller.OderServlet")
 public class OderServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
