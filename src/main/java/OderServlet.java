58e326f9a816892f41b65da5e789519b98cc4469
Le van son <levansoon>
2022-05-26 16:27:07 +0700

    sonlan1

---
+++ src/main/java/OderServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "OderServlet", value = "/OderServlet")
+public class OderServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
